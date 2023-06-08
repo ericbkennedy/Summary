a992332f2a96ddba7565d51bebf3bd1b0863a448
Eric Kennedy <ericbkennedy@gmail.com>
2023-06-07 19:05:31 -0700

    Initial Commit

---
+++ Summary/SummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryApp.swift
+//  Summary
+//
+//  Created by Eric Kennedy on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
